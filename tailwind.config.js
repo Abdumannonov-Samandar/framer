4456054dddc6783724b1701d27b1696a2892cb0f
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2024-03-13 22:11:09 +0500

    TESTIMIONAL  + SIGN UP build section

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     boxShadow: {
       'bg-sh' : '0px 4px 64px 0px rgba(0, 0, 0, 0.25)',
       'shadiw' : '0px 7px 24px 0px rgb(36, 38, 43)',
-      'btn-shadow' : '10px 10px 25px -10px rgba(54, 83, 107, 0.25)',
+      'btn-shadow' : '0px 7px 14px 0px rgb(234, 234, 234)',
     },
   },
   plugins: [],
